972b72c6f163c117a3b15d04d739414518e239db
Ysungho <ysh01022320@gmail.com>
2023-08-20 15:05:39 +0900

    프로젝트 완료 후 깃허브 등록. 해당 프로젝트는 배포 완료함

---
+++ src/test/java/com/mysite/sbb/SbbApplicationTest.java
@@ -0,0 +1,7 @@
+package com.mysite.sbb;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SbbApplicationTest {
+
+}
---
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -0,0 +1,6 @@
+package com.mysite.sbb;
+
+import static org.junit.jupiter.api.Assertions.*;
+class SbbApplicationTests {
+
+}
